7b0913d5ecef35cfe2cdf4de296bdea865c933fb
elchaowang <elcw@leeds.ac.uk>
2019-05-31 16:06:28 +0100

    added the GAN model which could be used to generate signals

--- test.py
+++ test.py
@@ -1,9 +1,63 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+# import glob
+# import imageio
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import os
+# import PIL
+# from tensorflow.keras import layers
+# import time
+# from IPython import display
+TF_CPP_MIN_LOG_LEVEL=2
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
+
+print(type(train_images))
+print(train_images.shape)
 
 
 
-La = [1, 2, 3, 4, 5, 6, 7, 8]
 
-x = La[2:5]
 
-print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
